6156f9427295e59ac09e1c778a73931c11ca99ae
HelixW <shreyas.2000@hotmail.com>
2019-12-23 22:49:01 +0530

    Updated README.md.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# restructured-color-game
-Just a regular RGB guessing game.
+# Color Pick
+
+![Color Pick Logo](https://img.icons8.com/color/96/000000/color-palette.png 'Color Pick')
+
+```
+Developed by Shreyas Khan --> (github.com/helixw)
+```
+
+[Link To Game](https://helixw.github.io/restructured-color-game)
+
+## _Description_
+
+`Color Pick` is a color guessing game where the player picks colors based on their intuition and RGB guessing skills.
+
+## _How To Play_
+
+-   A color is given in the form of a RGB code in the instruction part of the page.
+-   The player is supposed to guess the color and click the squares with colors.
+-   If the wrong color is chosen, the square disappears. The player can continue playing until the correct square is chosen.
+-   The player can click the `Change Colors` button if they wish to get a new set of colors. This will also generate a new color to guess.
+
+## _Modes Of Gameplay_
+
+1. `Easy Mode` : The player needs to guess from 3 squares.
+2. `Hard Mode` : The player needs to guess from 6 squares.
+
+## _How It Works_
+
+1. A set of 6 colors are generated with a random number generator.
+2. A color is chosen out of the 6 again with the help of the random number generator. This color is saved as the `Chosen Color`.
+3. The squares are filled with the generated colors and the `Color Hint` is changed to show the `Chosen Color`.
+
+## _Acknowledgements_
+
+-   This project is not an idea of my own but is a part of the course The Web Developer Bootcamp by Colt Steele.
--- script.js
+++ script.js
@@ -13,13 +13,13 @@ var ColorGame = {
         bottomRow: $('#bottom-row')
     },
 
-    init: function() {
+    init: function () {
         s = this.settings;
         this.generateColors();
         this.bindUIActions();
     },
 
-    generateColors: function() {
+    generateColors: function () {
         s.colors = [];
         this.toggleBottom(s.gameMode);
         for (var i = 0; i < s.gameMode; i++)
@@ -28,38 +28,38 @@ var ColorGame = {
         this.assignColors();
     },
 
-    generateRandom: function() {
+    generateRandom: function () {
         var r = Math.floor(Math.random() * 256);
         var g = Math.floor(Math.random() * 256);
         var b = Math.floor(Math.random() * 256);
         return 'rgb(' + r + ', ' + g + ', ' + b + ')';
     },
 
-    selectColor: function() {
+    selectColor: function () {
         return s.colors[Math.floor(Math.random() * s.gameMode)];
     },
 
-    assignColors: function() {
+    assignColors: function () {
         var i = 0;
-        s.squares.each(function() {
+        s.squares.each(function () {
             $(this).css('background-color', s.colors[i]);
             i++;
         });
         this.assignName();
     },
 
-    assignName: function() {
+    assignName: function () {
         s.namedColor.text(s.pickedColor);
     },
 
-    bindUIActions: function() {
-        s.squares.click(function() {
+    bindUIActions: function () {
+        s.squares.click(function () {
             $(this).css('background-color') === s.pickedColor
                 ? ColorGame.win()
                 : ColorGame.retry($(this));
         });
         s.resetButton.click(ColorGame.resetGame);
-        s.difficultyButtons.click(function() {
+        s.difficultyButtons.click(function () {
             ColorGame.selectButton($(this));
             $(this).attr('id') === 'easy-btn'
                 ? (s.gameMode = 3)
@@ -68,31 +68,31 @@ var ColorGame = {
         });
     },
 
-    win: function() {
+    win: function () {
         s.squares.removeClass('color-selected');
         s.squares.css('background-color', s.pickedColor);
         s.header.css('background-color', s.pickedColor);
         s.messageDisplay.text('Well Done!');
     },
 
-    retry: function(sqr) {
+    retry: function (sqr) {
         sqr.addClass('color-selected');
         s.messageDisplay.text('Try Again!');
     },
 
-    resetGame: function() {
+    resetGame: function () {
         s.squares.removeClass('color-selected');
         ColorGame.generateColors();
         s.messageDisplay.text('');
         s.header.css('background-color', 'steelblue');
     },
 
-    selectButton: function(btn) {
+    selectButton: function (btn) {
         s.difficultyButtons.removeClass('button-selected');
         btn.addClass('button-selected');
     },
 
-    toggleBottom: function(n) {
+    toggleBottom: function (n) {
         n === 3
             ? s.bottomRow.addClass('hide-row')
             : s.bottomRow.removeClass('hide-row');
